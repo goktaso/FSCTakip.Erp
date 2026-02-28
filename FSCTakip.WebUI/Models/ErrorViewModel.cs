8a312b8c9fc9c25ba7e7936f94acffec5cf40f1e
ozay <ozaygoktas@hotmail.com>
2026-03-01 01:31:47 +0300

    Proje dosyası ekle.

---
+++ FSCTakip.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FSCTakip.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
