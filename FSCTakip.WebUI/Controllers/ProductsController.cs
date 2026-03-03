f6bc9aa8f92284252eefa75c499a3a8e22053ad3
ozay.goktas <ogoktas@gokhanboya.com>
2026-03-03 10:01:39 +0300

    Added Save and Delete functionality for BagType in ProductsController, updated _StandardButtons for improved interaction, and enhanced BagTypes view layout.

--- FSCTakip.WebUI/Controllers/ProductsController.cs
+++ FSCTakip.WebUI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using FSC_ERP.Controllers;
+using FSC_ERP.Controllers;
+using FSCTakip.Core.Entities;
 using FSCTakip.DataAc.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,40 @@ public class ProductController : BaseController // Base'den türüyor
     {
         var list = await _context.BagTypes.ToListAsync();
         return View(list);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SaveBagType(BagType model)
+    {
+        if (model.Id == 0)
+        {
+            model.IsActive = true;
+            _context.BagTypes.Add(model);
+        }
+        else
+        {
+            _context.BagTypes.Update(model);
+        }
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(BagTypes));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteBagType(int id)
+    {
+        try
+        {
+            var entity = await _context.BagTypes.FindAsync(id);
+            if (entity == null)
+                return Json(new { success = false, message = "Kayıt bulunamadı." });
+            _context.BagTypes.Remove(entity);
+            await _context.SaveChangesAsync();
+            return Json(new { success = true, message = "Torba tipi başarıyla silindi." });
+        }
+        catch (Exception ex)
+        {
+            return Json(new { success = false, message = "Hata: " + ex.Message });
+        }
     }
 
     // Excel için sadece tek satır!
